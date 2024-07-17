2bc759ff7f91baefee2f1e8368c74f54e09bde6d
dave-stronghold <davestronghold@hotmail.com>
2024-07-18 01:00:37 +0530

    loop_bg

--- app/compnents/videoplayer.js
+++ app/compnents/videoplayer.js
@@ -1,154 +1,68 @@
 "use client";
-import React, { useRef, useState, useEffect, useCallback } from "react";
-import videojs from "video.js";
-import "video.js/dist/video-js.css";
+import React, { useState, useEffect } from "react";
 import Thumbnails from "./thumbnails";
 
 const VideoPlayer = ({ video, actions }) => {
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
-  const [currentVideo, setCurrentVideo] = useState(video[0]);
+  const [vdo, setvdo] = useState(video[0]);
+  const [vdo_b, setvdo_b] = useState(video[0]);
   const [transitionRunning, setTransitionRunning] = useState(false);
-  const [paintCount, setPaintCount] = useState(0);
-  const [startTime, setStartTime] = useState(0.0);
-  const fpsInfoRef = useRef(null);
-  const metadataInfoRef = useRef(null);
-  const requestIdRef = useRef(null); // To store the requestId of the callback
   const [action, setAction] = useState(null);
-  const playerRef = useRef(null);
 
-  const updateCanvas = useCallback(
-    (now, metadata) => {
-      const videoElement = videoRef.current;
-      const canvasElement = canvasRef.current;
-      const ctx = canvasElement.getContext("2d");
-
-      if (startTime === 0.0) {
-        setStartTime(now);
-      }
-
-      ctx.drawImage(
-        videoElement,
-        0,
-        0,
-        canvasElement.width,
-        canvasElement.height
-      );
-
-      const elapsed = (now - startTime) / 1000.0;
-      const fps = ((paintCount + 1) / elapsed).toFixed(3);
-      if (fpsInfoRef.current) {
-        fpsInfoRef.current.innerText = `video fps: ${fps}`;
-      }
-      if (metadataInfoRef.current) {
-        metadataInfoRef.current.innerText = JSON.stringify(metadata, null, 2);
-      }
-
-      setPaintCount(paintCount + 1);
-      requestIdRef.current =
-        videoElement.requestVideoFrameCallback(updateCanvas);
-    },
-    [startTime, paintCount]
-  );
-
-  useEffect(() => {
-    const videoElement = videoRef.current;
-    if (!videoElement) return;
-    console.log(currentVideo.loop)
-    const player = videojs(videoElement, {
-      controls: false,
-      autoplay: true,
-      preload: "auto",
-      sources: [{ src: currentVideo.path, type: "video/mp4" }],
-    });
-
-    playerRef.current = player;
-
-    player.on("loadeddata", () => {
-      player.play();
-      setPaintCount(0);
-      setStartTime(0.0);
-    });
-
-    return () => {
-      if (player) {
-        player.dispose();
-      }
-    };
-  }, [currentVideo]);
-
-  useEffect(() => {
-    if ("requestVideoFrameCallback" in HTMLVideoElement.prototype) {
-      const videoElement = videoRef.current;
-      requestIdRef.current =
-        videoElement.requestVideoFrameCallback(updateCanvas);
-
-      return () => {
-        if (requestIdRef.current) {
-          videoElement.cancelVideoFrameCallback(requestIdRef.current);
-        }
-      };
-    } else {
-      alert("Your browser does not support requestVideoFrameCallback().");
-    }
-  }, [updateCanvas]);
-
-  const changeVideo = (newVideo) => {
-    setCurrentVideo(newVideo);
-    if (playerRef.current) {
-      playerRef.current.src({ src: newVideo.path, type: "video/mp4" });
-      playerRef.current.load();
-    }
+  const handleVideoStart = () => {
+    vdo.to!=undefined?setvdo_b(video[vdo.to]):null;
+ 
   };
-
   const handleVideoEnd = () => {
-    setCurrentVideo(video[currentVideo.to]);
+ 
+    setvdo(video[vdo.to]);
   };
 
   const handleAction = (action) => {
-    if (action?.transitions[currentVideo?.type]) {
-      setCurrentVideo(video[action?.transitions[currentVideo?.type]]);
+    if (action?.transitions[vdo?.type]) {
+      setvdo(video[action?.transitions[vdo?.type]]);
     }
     setAction(action);
   };
 
   useEffect(() => {
-    if (currentVideo?.to !== undefined) {
+    console.log(vdo.to)
+    if (vdo?.to != undefined) {
       setTransitionRunning(true);
     } else {
       setTransitionRunning(false);
     }
-  }, [currentVideo]);
-
+  }, [vdo]);
   return (
     <div className="relative overflow-hidden w-screen h-screen">
-      <canvas
-        className="aspect-video w-full absolute"
-        ref={canvasRef}
-        width={1920}
-        height={1080}
+      <video
+        muted
+        x5-playsinline=""
+        playsInline
+        disablePictureInPicture
+        webkit-playsinline=""
+        controlsList="nodownload nofullscreen noremoteplayback"
+        autoPlay
+        className="absolute aspect-video w-full"
+        // className=" aspect-video w-96"
+        src={vdo.path}
+        loop={vdo.loop}
+        onPlaying={handleVideoStart}
+        onEnded={handleVideoEnd}
       />
       <video
-        ref={videoRef}
-        className="video-js vjs-hidden"
         muted
-        loop={currentVideo.loop}
+        x5-playsinline=""
         playsInline
-        onEnded={handleVideoEnd}
         disablePictureInPicture
+        webkit-playsinline=""
         controlsList="nodownload nofullscreen noremoteplayback"
+        autoPlay
+        // className=" aspect-video w-96"
+        className=" aspect-video w-full"
+        src={vdo_b.path}
+        loop={true}
       />
-      <div className="hidden">
-        {video.map((video, index) => (
-          <button
-            className="p-1 m-1 bg-blue-800"
-            key={index}
-            onClick={() => changeVideo(video)}
-          >
-            V{index + 1}
-          </button>
-        ))}
-      </div>
+
       <div className="absolute top-0 w-1/2">
         <Thumbnails
           actions={actions}
@@ -157,15 +71,11 @@ const VideoPlayer = ({ video, actions }) => {
         />
         <div className="hidden md:block">
           <div className="text-fuchsia-400">
-            {currentVideo.type ? currentVideo.type : "Transition"}
+            {vdo.type ? vdo.type : "Transition"}
           </div>
           <div>{transitionRunning.toString()}</div>
           <div className=" text-yellow-200">
-            {JSON.stringify(action?.transitions[currentVideo.type])}
-          </div>
-          <div className="mt-2">
-            <div ref={fpsInfoRef}></div>
-            <pre ref={metadataInfoRef}></pre>
+            {JSON.stringify(action?.transitions[vdo.type])}
           </div>
         </div>
       </div>
