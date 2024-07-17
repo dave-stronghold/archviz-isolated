e558afee5832ff98b79c38b5e65a3accba2aaaf1
dave-stronghold <davestronghold@hotmail.com>
2024-07-17 20:04:12 +0530

    avc

--- app/compnents/videoplayer2.js
+++ app/compnents/videoplayer2.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef } from "react";
 
-const sampleVideo = "/videos/sample.mp4";
+const sampleVideo = "/videos/sample.webm";
 const VideoPlayer = () => {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
@@ -11,7 +11,7 @@ const VideoPlayer = () => {
     const ctx = canvas.getContext("2d");
     const video = videoRef.current;
 
-    ctx.fillStyle = "green";
+    ctx.fillStyle = "blue";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     const drawFrame = () => {
       ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
@@ -23,18 +23,21 @@ const VideoPlayer = () => {
     <>
       <video
         ref={videoRef}
-        className="w-96 aspect-video"
+        className="w-96 aspect-video "
         muted
         x5-playsinline
         playsInline
         disablePictureInPicture
         webkit-playsinline
-        
+        loop
         controlsList="nodownload nofullscreen noremoteplayback"
         autoPlay
-        src={sampleVideo}
+        // src={sampleVideo}
+        src='/videos/test2.mp4'
+        // src='/videos/swimming_pool/Swim_2-hero_2.mp4'
       />
       <canvas ref={canvasRef} width={384} height={216} />
+      {/* <video src='/videos/test.mp4' width={400} controls autoPlay loop muted playsInline></video> */}
     </>
   );
 };
